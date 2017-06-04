a432c1b7df4e9f177626e21b3823ffa19307a3af
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-06-04 10:08:37 -0400

    Fix default source directory to align with documentation

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,7 +7,7 @@ import (
 // ParseOptions parses the command line options.
 // Returns the parsed options.
 func ParseOptions() Options {
-	var sourceDir = flag.String("sourceDir", "source", "The source directory to search for AsciiDoc files")
+	var sourceDir = flag.String("sourceDir", "content", "The source directory to search for AsciiDoc files")
 	var fail = flag.Bool("fail", true, "Fails the program if at least one issue was found")
 	flag.Parse()
 	return Options{SourceDir: *sourceDir, Fail: *fail}
