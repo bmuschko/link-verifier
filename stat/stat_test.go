d4691ba32c0a9e9b8a543f01bbe2afa3f2c0a4b3
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-06-04 08:40:05 -0400

    Run gofmt

--- stat/stat_test.go
+++ stat/stat_test.go
@@ -1,39 +1,38 @@
 package stat
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestSumSuccessesForEmptySlice(t *testing.T) {
-    aggregateSummary := []Summary{}
-    sum := SumSuccesses(aggregateSummary)
-    assert.Equal(t, 0, sum)
+	aggregateSummary := []Summary{}
+	sum := SumSuccesses(aggregateSummary)
+	assert.Equal(t, 0, sum)
 }
 
 func TestSumSuccessesForPopulatedSlice(t *testing.T) {
-    aggregateSummary := summaries()
-    sum := SumSuccesses(aggregateSummary)
-    assert.Equal(t, 29, sum)
+	aggregateSummary := summaries()
+	sum := SumSuccesses(aggregateSummary)
+	assert.Equal(t, 29, sum)
 }
 
 func TestSumFailuresForEmptySlice(t *testing.T) {
-    aggregateSummary := []Summary{}
-    sum := SumFailures(aggregateSummary)
-    assert.Equal(t, 0, sum)
+	aggregateSummary := []Summary{}
+	sum := SumFailures(aggregateSummary)
+	assert.Equal(t, 0, sum)
 }
 
 func TestSumFailuresForPopulatedSlice(t *testing.T) {
-    aggregateSummary := summaries()
-    sum := SumFailures(aggregateSummary)
-    assert.Equal(t, 70, sum)
+	aggregateSummary := summaries()
+	sum := SumFailures(aggregateSummary)
+	assert.Equal(t, 70, sum)
 }
 
 func summaries() []Summary {
-    summaries := []Summary{}
-    summaries = append(summaries, Summary{Successful: 20, Failed: 3})
-    summaries = append(summaries, Summary{Successful: 7, Failed: 67})
-    summaries = append(summaries, Summary{Successful: 2, Failed: 0})
-    return summaries
+	summaries := []Summary{}
+	summaries = append(summaries, Summary{Successful: 20, Failed: 3})
+	summaries = append(summaries, Summary{Successful: 7, Failed: 67})
+	summaries = append(summaries, Summary{Successful: 2, Failed: 0})
+	return summaries
 }
-
--- verify/verify.go
+++ verify/verify.go
@@ -1,83 +1,83 @@
 package verify
 
 import (
-    "github.com/bmuschko/asciidoc-link-verifier/stat"
-    "github.com/bmuschko/asciidoc-link-verifier/file"
-    "github.com/bmuschko/asciidoc-link-verifier/text"
-    "github.com/bmuschko/asciidoc-link-verifier/http"
-    "fmt"
-    "strconv"
-    "os"
+	"fmt"
+	"github.com/bmuschko/asciidoc-link-verifier/file"
+	"github.com/bmuschko/asciidoc-link-verifier/http"
+	"github.com/bmuschko/asciidoc-link-verifier/stat"
+	"github.com/bmuschko/asciidoc-link-verifier/text"
+	"os"
+	"strconv"
 )
 
 // Resolve resolves AsciiDoc files for a given directory.
 // Returns resolved AsciiDoc files.
 func Resolve(sourceDir string) []string {
-    fmt.Println("Searching AsciiDoc files in directory:", sourceDir)
-    return file.FindAsciiDocFiles(sourceDir)
+	fmt.Println("Searching AsciiDoc files in directory:", sourceDir)
+	return file.FindAsciiDocFiles(sourceDir)
 }
 
 // Process processes AsciiDoc files by verifying each parsed links by emitting a HTTP call.
 // Prints out a summary of successful and failed links.
 // By default fails the program if at least one link could not be resolved.
 func Process(asciiDocFiles []string, fail bool) {
-    aggregateSummary := []stat.Summary{}
+	aggregateSummary := []stat.Summary{}
 
-    for _, asciiDocFile := range asciiDocFiles {
-        fmt.Println()
-        fmt.Println("-> Verifying file:", asciiDocFile)
-        content := file.ReadFile(asciiDocFile)
-        summary := parseLinks(content)
-        aggregateSummary = append(aggregateSummary, summary)
-    }
+	for _, asciiDocFile := range asciiDocFiles {
+		fmt.Println()
+		fmt.Println("-> Verifying file:", asciiDocFile)
+		content := file.ReadFile(asciiDocFile)
+		summary := parseLinks(content)
+		aggregateSummary = append(aggregateSummary, summary)
+	}
 
-    if len(aggregateSummary) > 0 {
-        successCount := stat.SumSuccesses(aggregateSummary)
-        failureCount := stat.SumFailures(aggregateSummary)
-        stats := fmt.Sprintf("SUCCESSFUL: %s, FAILED: %s", strconv.Itoa(successCount), strconv.Itoa(failureCount))
-        fmt.Println()
-        fmt.Println(calculateSeparator(stats))
-        fmt.Println(stats)
+	if len(aggregateSummary) > 0 {
+		successCount := stat.SumSuccesses(aggregateSummary)
+		failureCount := stat.SumFailures(aggregateSummary)
+		stats := fmt.Sprintf("SUCCESSFUL: %s, FAILED: %s", strconv.Itoa(successCount), strconv.Itoa(failureCount))
+		fmt.Println()
+		fmt.Println(calculateSeparator(stats))
+		fmt.Println(stats)
 
-        if failureCount > 0 && !fail {
-            os.Exit(1)
-        }
-    }
+		if failureCount > 0 && !fail {
+			os.Exit(1)
+		}
+	}
 }
 
 func parseLinks(content string) stat.Summary {
-    links := text.ParseLinks(content)
-    summary := stat.Summary{Successful: 0, Failed: 0}
+	links := text.ParseLinks(content)
+	summary := stat.Summary{Successful: 0, Failed: 0}
 
-    if len(links) == 0 {
-        fmt.Println("No links found.")
-    }
+	if len(links) == 0 {
+		fmt.Println("No links found.")
+	}
 
-    for _, link := range links {
-        validateLink(link, &summary)
-    }
+	for _, link := range links {
+		validateLink(link, &summary)
+	}
 
-    return summary
+	return summary
 }
 
 func validateLink(link string, summary *stat.Summary) {
-    response := http.Get(link)
+	response := http.Get(link)
 
-    if response.Success {
-        summary.Successful++
-        fmt.Println("[OK] " + link)
-    } else {
-        summary.Failed++
-        fmt.Println("[FAILED] " + link + " (" + response.Status + ")")
-    }
+	if response.Success {
+		summary.Successful++
+		fmt.Println("[OK] " + link)
+	} else {
+		summary.Failed++
+		fmt.Println("[FAILED] " + link + " (" + response.Status + ")")
+	}
 }
 
 func calculateSeparator(stats string) string {
-    var separator = ""
+	var separator = ""
 
-    for i := 0; i < len(stats); i++ {
-        separator += "-"
-    }
+	for i := 0; i < len(stats); i++ {
+		separator += "-"
+	}
 
-    return separator
+	return separator
 }
