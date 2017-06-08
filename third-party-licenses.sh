ca475fb82a9ef1f440651f3f0e3cb5377e113b58
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-06-07 20:51:24 -0400

    Add script for merging third-party licenses

---
+++ third-party-licenses.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+MERGE_FILE="build/THIRD-PARTY-LICENSES"
+
+rm -f ${MERGE_FILE}
+
+echo "Third party libraries" >> ${MERGE_FILE}
+echo "=====================" >> ${MERGE_FILE}
+
+licenseFiles=(`find vendor -name 'LICENSE'`)
+
+for i in "${licenseFiles[@]}"
+do
+   :
+   echo "" >> ${MERGE_FILE}
+   path="${i#vendor/}"
+   len=${#path}
+   echo ${path} >> ${MERGE_FILE}
+   underscore=$(printf "%0.s=" $(seq 1 $len))
+   echo ${underscore} >> ${MERGE_FILE}
+   echo "" >> ${MERGE_FILE}
+   cat ${i} >> ${MERGE_FILE}
+done
