4d8254f29340c8a61fb6c669e018d60303bce398
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-06-10 13:37:32 -0400

    Fix format of comment

--- text/parse.go
+++ text/parse.go
@@ -1,4 +1,4 @@
-// This package uses the library "github.com/mvdan/xurls" to parse URLs from a string.
+// Package text uses the library "github.com/mvdan/xurls" to parse URLs from a string.
 // The library is distributed with the license BSD 3-clause.
 // Copyright (c) 2015, Daniel Martí. All rights reserved.
 package text
