04d3630022ef0d926c17086d759fee125c2c9713
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-07-26 22:36:07 -0400

    Wrapper and additional tasks

---
+++ buildSrc/src/main/groovy/AggregateCoverage.groovy
@@ -0,0 +1,28 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputFile
+
+class AggregateCoverage extends DefaultTask {
+    @InputDirectory
+    File inputDir = project.file('.gogradle/reports/coverage/profiles')
+    
+    @OutputFile
+    File outputFile = project.file("$project.buildDir/reports/coverage-aggregate/coverage.txt")
+    
+    AggregateCoverage() {
+        group = 'GoGradle'
+        description = 'Aggregates coverage profile data.'
+    }
+    
+    @TaskAction
+    void aggregate() {
+        StringBuilder aggregatedCoverage = new StringBuilder()
+        
+        inputDir.listFiles().each {
+            aggregatedCoverage << it.text
+        }
+        
+        outputFile.text = aggregatedCoverage.toString()
+    }
+}
---
+++ buildSrc/src/main/groovy/Distributions.groovy
@@ -0,0 +1,52 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.Project
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
+
+class Distributions extends DefaultTask {
+    @InputDirectory
+    File inputDir = project.file('.gogradle')
+    
+    @OutputDirectory
+    File outputDir = project.file("$project.buildDir/distributions")
+    
+    Distributions() {
+        group = 'GoGradle'
+        description = 'Builds packaged distributions for all prebuilt binaries.'
+    }
+    
+    @TaskAction
+    void create() {
+        inputDir.listFiles(new CrossCompiledFileFilter(project)).each { preBuiltLib ->
+            if (preBuiltLib.name.contains('windows')) {
+                ant.zip(destfile: "${outputDir}/${preBuiltLib.name}.zip") {
+                    fileset(dir: inputDir) {
+                        include(name: preBuiltLib.name)
+                    }
+                }
+            } else {
+                def tarFile = "${outputDir}/${preBuiltLib.name}.tar"
+                ant.tar(destfile: tarFile) {
+                    tarfileset(dir: inputDir) {
+                        include(name: preBuiltLib.name)
+                    }
+                }
+                ant.gzip(destfile: "${tarFile}.gz", src: tarFile)
+                project.delete(tarFile)
+            }
+        }
+    }
+    
+    private static class CrossCompiledFileFilter implements FilenameFilter {
+        private final Project project
+        
+        CrossCompiledFileFilter(Project project) {
+            this.project = project
+        }
+        
+        boolean accept(File f, String filename) {
+            filename.endsWith("_${project.name}")
+        }  
+    } 
+}
