95fa9abdb284f37373b8e81e49e7ad1070ca4e08
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-07-29 15:23:47 -0400

    Moved custom tasks into package

--- buildSrc/src/main/groovy/com/bmuschko/linkverifier/AggregateCoverage.groovy
+++ buildSrc/src/main/groovy/com/bmuschko/linkverifier/AggregateCoverage.groovy
@@ -1,3 +1,5 @@
+package com.bmuschko.linkverifier
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.InputDirectory
--- buildSrc/src/main/groovy/com/bmuschko/linkverifier/Distributions.groovy
+++ buildSrc/src/main/groovy/com/bmuschko/linkverifier/Distributions.groovy
@@ -1,3 +1,5 @@
+package com.bmuschko.linkverifier
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
