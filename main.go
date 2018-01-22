9dbb3c5a8b694965521ca1e54c205d53de3ef5d5
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-01-22 07:18:14 -0700

    Optimize imports

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/bmuschko/link-verifier/cmd"
-	"github.com/bmuschko/link-verifier/verify"
 	"github.com/bmuschko/link-verifier/http"
+	"github.com/bmuschko/link-verifier/verify"
 )
 
 func main() {
