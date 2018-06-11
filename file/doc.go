ed65f019e60fd2d8d53ff5743b0c660d54277029
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-06-11 11:26:28 -0600

    Fix formatting

--- file/doc.go
+++ file/doc.go
@@ -2,17 +2,17 @@ package file
 
 import "strings"
 
-var AsciiDoc = Document{"AsciiDoc", []string {"*.adoc", "*.asciidoc", "*.asc"}}
-var Markdown = Document{"Markdown", []string {"*.md", "*.markdown", "*.mdown"}}
-var PlainText = Document{"Plain Text", []string {"*.txt"}}
+var AsciiDoc = Document{"AsciiDoc", []string{"*.adoc", "*.asciidoc", "*.asc"}}
+var Markdown = Document{"Markdown", []string{"*.md", "*.markdown", "*.mdown"}}
+var PlainText = Document{"Plain Text", []string{"*.txt"}}
 
 type Document struct {
 	name string
-	ext []string
+	ext  []string
 }
 
 // JoinedExt joins all registered file extensions of a document.
 // Returns the joined extensions of a document separated by comma.
 func (d Document) JoinedExt() string {
-	return strings.Join(d.ext,",")
+	return strings.Join(d.ext, ",")
 }
