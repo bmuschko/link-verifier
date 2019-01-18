88242bdbd77ca7e124bdb90b21029a268966667a
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-01-18 16:21:57 -0700

    Vendor directory is excluded with Go 1.9 and up

--- test.sh
+++ test.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-set -e
-echo "" > coverage.txt
-
-for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
+go test ./... -coverprofile=coverage.txt -covermode=atomic
+go tool cover -html=coverage.txt
