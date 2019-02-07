4639b9e6cd463d4139ee5825de3802f0bbeed08d
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-02-07 12:44:15 -0700

    Align naming of parameter with others
    The parameter name should indicate a plural of things.

--- cmd/root.go
+++ cmd/root.go
@@ -19,7 +19,7 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	rootCmd.PersistentFlags().StringSliceVar(&parsedOptions.RootDirs, "dirs", []string{"."}, "comma-separated root directories used to recursively search for files")
-	rootCmd.PersistentFlags().StringSliceVar(&parsedOptions.IncludePatterns, "include", []string{"*.md", "*.adoc"}, "comma-separated root directories used to recursively search for files")
+	rootCmd.PersistentFlags().StringSliceVar(&parsedOptions.IncludePatterns, "includes", []string{"*.md", "*.adoc"}, "comma-separated root directories used to recursively search for files")
 	rootCmd.PersistentFlags().IntSliceVar(&parsedOptions.IgnoreStatusCodes, "ignore-status-codes", []int{}, "comma-separated HTTP response codes that should be ignored for validation")
 	rootCmd.PersistentFlags().BoolVar(&parsedOptions.Fail, "fail", true, "fails the program if at least one discovered link cannot be resolved")
 	rootCmd.PersistentFlags().IntVar(&parsedOptions.Timeout, "timeout", 5, "timeout in seconds used when calling the URL")
